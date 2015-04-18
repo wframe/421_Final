2515736c2a616f189aefa5752195736386d622e1
wframe <williamrframe@gmail.com>
2015-04-17 19:12:10 -0500

    more rules

--- pos.py
+++ pos.py
@@ -1,5 +1,6 @@
 import nltk, string
 
 def get_sentence_tags(sentence): 
-    return [x[1] for x in nltk.pos_tag(nltk.word_tokenize(sentence.translate(string.maketrans("",""), string.punctuation) ))    ]
+    #return [x[1] for x in nltk.pos_tag(nltk.word_tokenize(sentence.translate(string.maketrans("",""), string.punctuation) ))    ]
+    return [x[1] for x in nltk.pos_tag(nltk.word_tokenize(sentence))    ]
 
--- test.py
+++ test.py
@@ -10,27 +10,29 @@ def previous_and_next(some_iterable):
     return izip(prevs, items, nexts)
     
 def test_tag_performance(directory):
-    lofis = fi.recGetTextFiles(r'C:\Users\William\CS421Proj\421_Final\essays\original')
+    lofis = fi.recGetTextFiles(r'C:\Users\William\CS421Proj\421_Final\essays\original\low')
     mefis = fi.recGetTextFiles(r'C:\Users\William\CS421Proj\421_Final\essays\original\medium')
     hifis = fi.recGetTextFiles(r'C:\Users\William\CS421Proj\421_Final\essays\original\high')
     loscore = 0
-    for file in lofis:
-        filescore = 0
-        text = open(file,'r')
-        for sent in gr.get_sentences(text.read()):
-            tags = pos.get_sentence_tags(sent)
-            filescore += pos_score(tags)
-        loscore += filescore
-    print("low pos score: " + str(loscore))
-    mescore = 0
-    for file in mefis:
-        filescore = 0
-        text = open(file,'r')
-        for sent in gr.get_sentences(text.read()):
-            tags = pos.get_sentence_tags(sent)
-            filescore += pos_score(tags)
-        mescore += filescore
-    print("med pos score: " + str(mescore))
+#==============================================================================
+#     for file in lofis:
+#         filescore = 0
+#         text = open(file,'r')
+#         for sent in gr.get_sentences(text.read()):
+#             tags = pos.get_sentence_tags(sent)
+#             filescore += pos_score(tags)
+#         loscore += filescore
+#     print("low pos score: " + str(loscore))
+#     mescore = 0
+#     for file in mefis:
+#         filescore = 0
+#         text = open(file,'r')
+#         for sent in gr.get_sentences(text.read()):
+#             tags = pos.get_sentence_tags(sent)
+#             filescore += pos_score(tags)
+#         mescore += filescore
+#     print("med pos score: " + str(mescore))
+#==============================================================================
 
     hiscore = 0
     for file in hifis:
@@ -40,16 +42,26 @@ def test_tag_performance(directory):
             tags = pos.get_sentence_tags(sent)
             filescore += pos_score(tags)
         hiscore += filescore
+        print(filescore)
     print("hi pos score: " + str(hiscore))        
         
 def pos_score(tags):
-    errors = 0    
-    for previous, current, nxt in     previous_and_next(tags):
+    errors = 0
+    present = True
+    if "VBN" in tags or "VBD" in tags:
+        present = False
+    presTags = ['VB','VBZ','VBP','VBG']
+    for previous, current, nxt in  previous_and_next(tags):
+        if not present and current in presTags:
+            errors += 1
         if current == "NN":
-            if nxt == "VBP" or nxt == "NNS":
+            if nxt == "VBP":
                 errors += 1
         if current == "NNS":
-            if nxt == "VBZ" or nxt == "NN":
+            if nxt == "VBZ":
+                errors += 1
+        if current == ",":
+            if (previous == "NN" and nxt == "NNS") or (previous == "NNS" and nxt == "NN"):
                 errors += 1
     return errors
         
