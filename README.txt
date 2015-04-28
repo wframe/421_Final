65546960d4894718b08f028d5bbb837dc8b489a8
maxfarnham <max.farnham@vizwit.com>
2015-04-28 00:42:19 -0500

    Submitted competition entry

--- README.txt
+++ README.txt
@@ -5,13 +5,16 @@ REQUIREMENTS:
 	TEXTBLOB
 		pip install textblob
 	STANFORD PARSER
-		-> download below and unpack into /src/scoring/stanford-parser/
+		-> download below and unpack into /src/scoring/stanford-parser/:
+			https://drive.google.com/file/d/0B_EGNlVgIoFTZXdqaXMyZURkM3c/view?usp=sharing
 
 		Update these values in /src/scoring/syntax.py:
 			-> ROOT_DIR - point this the directory where you've downloaded this project
 			-> JAVA_HOME - point this at the bin of your java installation. Make sure it's Java 8
 
+		Note that this version of our project is glacially slow, mostly due to the stanford parser
+
 EXECUTION:
 	Run:
 		python grader.py
-	This will run the grader on all files inside the provided directory
+	This will run the grader on all files inside the input/original directory
--- grader.py
+++ grader.py
@@ -26,69 +26,110 @@ lengthStats = (13.4667, 6.4924)
 
 
 def mean(numbers):
-    return sum(numbers)/float(len(numbers))
+	return sum(numbers)/float(len(numbers))
 def stdev(numbers):
 	avg = mean(numbers)
 	variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
 	return math.sqrt(variance)
 def computeZ(param, mu, dev):
-    return (param - mu)/dev
+	return (param - mu)/dev
 essay_path = 'essays/original/low'
 essay_path = path.abspath(essay_path)
 files = []
 
 def process_file(file_path):
-    misspelled = 0
-    misspellings = []
-    ovr_agreementscore = 0.0
-    ovr_verbscore = 0.0
-    with open(file_path, 'r') as f:
-        text = unicode(f.read())
-        t = TextBlob(text)
-        sents = t.sentences
-        cohesion = tc.coherence(t)
-        #get misspellings
-        gverbs = pos_global_verbs(t.tags)
-        for sent in sents:
-	        for word in sent.tokens:
-		        if len(word) > 0 and correct(word) != word:
-			        misspellings.append(word)
-			        misspelled += 1
+	misspelled = 0
+	misspellings = []
+	ovr_agreementscore = 0.0
+	ovr_verbscore = 0.0
+	with open(file_path, 'r') as f:
+		text = unicode(f.read())
+		t = TextBlob(text)
+		sents = t.sentences
+		cohesion = tc.coherence(t)
+		#get misspellings
+		gverbs = pos_global_verbs(t.tags)
+		for sent in sents:
+			for word in sent.tokens:
+				if len(word) > 0 and correct(word) != word:
+					misspellings.append(word)
+					misspelled += 1
 		#get agreement & verb scores
-        word_count = 0
-        agreementscore = 0.0
-        verbscore = 0.0
-        for sent in sents:
-            tags = tag_sent(sent.string)
-            agreementscore += pos_agreement(tags)/len(t.words)
-            verbscore += gverbs+ (pos_verbs(tags)/len(t.words))
+		word_count = 0
+		agreementscore = 0.0
+		verbscore = 0.0
+		for sent in sents:
+			tags = tag_sent(sent.string)
+			agreementscore += pos_agreement(tags)/len(t.words)
+			verbscore += gverbs+ (pos_verbs(tags)/len(t.words))
 
-        parse_score = 0#syntax.syntactic_score(text)
-        topic_score = topicality.topicality_score(text)
+		parse_score = 0#syntax.syntactic_score(text)
+		topic_score = topicality.topicality_score(text)
 
 
-	return (misspelled, agreementscore, verbscore, cohesion[0], cohesion[1], len(sents), parse_score, topic_score)
+	return [misspelled, agreementscore, verbscore, parse_score, cohesion[0], cohesion[1], len(sents)]
 
 if __name__ == '__main__':
-    returns = []
-    if len(sys.argv) > 1:
-	    essay_path = path.abspath(sys.argv[1])
-    misspelled = 0 
-    for (dirpath, dirnames, filenames) in walk(essay_path):
-	    files.extend(filenames)
-	    break
-    paths = ('input/original',)
-    #for testing
-    i = -1
-    with open(r'output\result.txt', 'w+') as out:
-        for p in paths:
-            print(p)
-            essay_path = path.abspath(p)
-            files = []
-            for (dirpath, dirnames, filenames) in walk(essay_path):
-	           files.extend(filenames)
-	           break
-            for fname in files:
-                tup = process_file(essay_path + '\\' + fname)
-                out.write("%s" % (tup,))
-                out.write('\n')
+	returns = []
+	if len(sys.argv) > 1:
+		essay_path = path.abspath(sys.argv[1])
+	misspelled = 0 
+	for (dirpath, dirnames, filenames) in walk(essay_path):
+		files.extend(filenames)
+		break
+	paths = ('input/original',)
+	#for testing
+	i = -1
+	with open(r'output\result.txt', 'w+') as out:
+		for p in paths:
+			print(p)
+			essay_path = path.abspath(p)
+			files = []
+			for (dirpath, dirnames, filenames) in walk(essay_path):
+			   files.extend(filenames)
+			   break
+			for fname in files:
+				print "processing {0}...".format(fname)
+				param = process_file(essay_path + '\\' + fname)
+
+				ret = process_file(essay_path + '\\' + fname)
+		
+				#precomputed stats based on training data
+				paramStats = []
+				spellingStats= (55.9833, 19.5114)
+				agreementStats = (.2059, .0934)
+				verbStats = (.06556, .03331)
+				parseStats =  (0, 1)
+				pronsStats = (-3.3233,6.0137)
+				topicalityStats = (.4816,.1227 )
+				lengthStats = (13.4667, 6.4924)
+				paramStats.append(spellingStats)
+				paramStats.append(agreementStats)
+				paramStats.append(verbStats)
+				paramStats.append(parseStats)
+				paramStats.append(pronsStats)
+				paramStats.append(topicalityStats)
+				paramStats.append(lengthStats)
+	
+				fscore = 0
+				fscores = []
+				label = 0
+				j = 0
+				for param in ret:
+					fscores.append(computeZ(param, paramStats[j][0], paramStats[j][1]))
+					j+=1
+				fscore +=  (-1*fscores[0]) + (-1*fscores[1]) + fscores[2] + (2*fscores[3]) + (2*fscores[4]) + (3*fscores[5]) + (-2*fscores[6])
+				if fscore < -3:
+					label = 0
+				elif fscore < 3.5:
+					label = 1
+				else:
+					label = 2
+
+				LABELS = ['low', 'medium', 'high']
+				out.write("{0:12}".format(fname))
+				for val in fscores:
+					if isinstance(val, (int, long, float, complex)):
+						out.write("{0:6} ".format(round(val, 2)))
+				out.write(LABELS[label])
+				out.write('\n')
